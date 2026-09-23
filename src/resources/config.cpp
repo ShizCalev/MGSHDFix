5fb6972abfb0994ff77774c17c03a176009eeca1
Paul Connolly <paulconnolly@gmail.com>
2026-09-23 01:36:12 +0100

    MGS2: wash spark and electricity light back off preshaded walls so it stops staining them

--- src/resources/config.cpp
+++ src/resources/config.cpp
@@ -90,6 +90,7 @@
 #include "mgs2_contrast_fix.hpp"
 #include "mgs2_ai_ray_vision.hpp"
 #include "mgs2_title_lightning.hpp"
+#include "mgs2_light_stain.hpp"
 #include "mgs2_newscrconcentrateblur.hpp"
 #include "mgs2_restore_dogtag_viewer.hpp"
 #include "mgs2_vamp_punch_fix.hpp"
@@ -768,6 +769,7 @@ void Config::Read()
                 &MGS2_ContrastShader::bEnabled,
                 &MGS2_AiRayVision::bEnabled,
                 &MGS2_TitleLightning::bEnabled,
+                &MGS2_LightStain::bEnabled,
                 &MGS2_Crossfade::bEnabled,
                 &MGS2ConcentrateBlur::bEnabled,
                 &MGS2RailgunBeam::bEnabled,
